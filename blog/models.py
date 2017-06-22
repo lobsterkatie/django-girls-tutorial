63ca75c0f11c6d632023652fec097f968c82ea10
Katie Byers <lobsterkatie@gmail.com>
2017-06-22 12:51:57 -0700

    added Post to models and migrated

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,27 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    """A blog post"""
+
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+
+    def publish(self):
+        """WHAT DO I DO?????"""
+
+        self.published_date = timezone.now()
+        self.save()
+
+
+    def __str__(self):
+        """Stringify the object"""
+
+        return self.title
